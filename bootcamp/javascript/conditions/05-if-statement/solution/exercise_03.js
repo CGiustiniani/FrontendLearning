10e79ef950e17bd1bb588c0df2be4cf55f3fe657
Matija Vuk <matija.vuk@ecx.io>
2022-03-29 13:04:02 +0200

    JS Conditions * added content for operators, truthy-falsy, logical operators, boolean algebra * added tasks & solutions for if statement & switch statement

---
+++ bootcamp/javascript/conditions/05-if-statement/solution/exercise_03.js
@@ -0,0 +1,12 @@
+// T = Temperature
+// D = Day
+// W = Weather
+//
+// Close the window if it's not `Wednesday` and the temperature is below `22`.
+
+let day = 'Monday';
+let temperature = 20;
+
+if (day !== 'Wednesday' && temperature < 22) {
+    console.log('Close the window.');
+}
