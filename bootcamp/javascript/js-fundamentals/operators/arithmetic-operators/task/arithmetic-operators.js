acd03a104365a5ed06b4e1e238335966d32a4cb7
Ivan Logožar <ivan.logozar@ecx.io>
2022-04-01 14:07:49 +0200

    -stackblitz adaptions

--- bootcamp/javascript/js-fundamentals/operators/arithmetic-operators/task/arithmetic-operators.js
+++ bootcamp/javascript/js-fundamentals/operators/arithmetic-operators/task/arithmetic-operators.js
@@ -2,17 +2,17 @@
 //
 // 1. What is the output of the following code?
 
-let x = 10;
-let y = 5;
+let a = 10;
+let b = 5;
 
-++x;
-// console.log(x++);
-// console.log(++y);
+++a;
+// console.log(a++);
+// console.log(++b);
 
 // 2. What is the output of the following code?
 
 let x = '30';
-let y = 10;
+let y= 10;
 
-// console.log(x + y);
 // console.log(x - y);
+// console.log(x + y);
