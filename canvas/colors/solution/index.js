dc1ffdd9acf5e28c4e9f2b0756ffadd41aca4adb
Christopher Giustiniani <christopher.giustiniani@ecx.io>
2022-10-25 15:05:52 +0200

    add canvas tasks

---
+++ canvas/colors/solution/index.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const colors = [
+  '#9400D3',
+  '#4B0082',
+  '#0000FF',
+  '#00FF00',
+  '#FFFF00',
+  '#FF7F00',
+  '#FF0000'
+];
+
+// Step 1
+// Use an aspect ratio of 3:2 (width/height) by setting the width and height for the canvas
+
+canvas.width = 330;
+canvas.height = 220;
+
+// Step 2
+// Loop through the colors array to createeven distributed rectangles
+
+colors.forEach((color, i) => {
+  ctx.fillStyle = colors[i];
+  ctx.fillRect(
+    0,
+    0 + i * (canvas.height / colors.length),
+    canvas.width,
+    canvas.height / colors.length
+  );
+});
---
+++ canvas/colors/task/index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// Step 1
+// Use an aspect ratio of 3:2 (width/height) by setting the width and height for the canvas.
+
+canvas.width = null;
+canvas.height = null;
+
+
+// Step 2
+// Place your shapes
+
+
