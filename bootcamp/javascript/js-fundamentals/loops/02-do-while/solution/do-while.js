65377f9d5aae01d3601e54e0ade4755ebc214d0a
anamarija.kukulj <anamarija.kukulj@ibmix.hr>
2023-03-06 10:56:13 +0100

    Add do..while task and solution

---
+++ bootcamp/javascript/js-fundamentals/loops/02-do-while/solution/do-while.js
@@ -0,0 +1,17 @@
+//#do...while
+
+//Draw a random number, if an existing one is drawn, draw again.
+
+const numbers = [];
+
+function draw() {
+    let number;
+
+    do {
+        number = getRandomNumber(0, 10);
+    } while(numbers.includes(number));
+    
+    numbers.push(number);
+    console.log(numbers);
+}
+
