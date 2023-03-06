48ab987f8dbaac8495f9291ccf0746a3dd415d53
anamarija.kukulj <anamarija.kukulj@ibmix.hr>
2023-03-06 11:07:00 +0100

    Add break soultion and task

---
+++ bootcamp/javascript/js-fundamentals/loops/03-break/solution/break.js
@@ -0,0 +1,9 @@
+//#break
+
+//The following example iterates through the elements in an array until it finds the index of an element whose value is theValue:
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === theValue) {
+        break;
+    }
+}
