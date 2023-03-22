58106322a83f672adaa590fe4e11a9302acce3d6
anamarija.kukulj <anamarija.kukulj@ibmix.hr>
2023-03-22 09:36:07 +0100

    Add exercises and solutions

---
+++ bootcamp/javascript/js-fundamentals/conditions/switch-statements/task/conditions-switch.js
@@ -0,0 +1,20 @@
+//Exercise 1
+
+let temperature = 21;
+let day = 'Monday';
+
+// if (temperature < 22) {
+//     switch (day) {
+//         case 'Monday':
+//             setBlindsPosition(25);
+//             break;
+//         case 'Wednesday':
+//             setBlindsPosition(30);
+//             break;
+//         case 'Friday':
+//             setBlindsPosition(50);
+//             break;
+//         default:
+//             setBlindsPosition(70);
+//     }
+// }
